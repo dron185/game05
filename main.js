1ef118cab57674d3945b6db88c3ef4c42c24ef9e
Andrei Huryn <gurin.nord@gmail.com>
2024-09-28 18:11:42 +0300

    feat: add index.html, front.js

---
+++ main.js
@@ -0,0 +1,5 @@
+// const game = new Game();
+//
+// const view = new View()
+//
+// const controller = new Controller(view, game)
---
+++ view.js
@@ -0,0 +1,22 @@
+// export class View {
+//     #rootEl;
+//
+//     constructor(rootId) {
+//         this.#rootEl = document.getElementById(rootId);
+//     }
+//
+//     render() {
+//         this.#rootEl.innerHTML = '';
+//     }
+// }
+//
+// export class Controller {
+//     #view;
+//     #model;
+//
+//     // Dependency Injections
+//     constructor(view, model) {
+//         this.#view = view;
+//         this.#model = model;
+//     }
+// }
